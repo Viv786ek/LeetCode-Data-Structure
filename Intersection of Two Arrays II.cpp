01cffa89a4cd04fabebdcd89a93067152fd02285
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-09-11 13:16:44 +0530

    Create  Intersection of Two Arrays II.cpp

---
+++ Intersection of Two Arrays II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>v;
+        map<int,int>m1,m2;
+        for(auto x:nums1){
+            m1[x]++;
+        }
+        for(auto X:nums2){
+            m2[X]++;
+        }
+        
+        for(int i=0;i<nums1.size();i++){
+            if(m1[nums1[i]] >0 && m2[nums1[i]]>0){
+                v.push_back(nums1[i]);
+                m1[nums1[i]]--;
+                m2[nums1[i]]--;
+            }
+        }
+        return v;
+    }
+};
